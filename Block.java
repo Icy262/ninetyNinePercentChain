15d67e85f12454b0f438ead2ed1d574bbd8dbd94
Icy26 <114495528+Icy262@users.noreply.github.com>
2024-04-25 10:25:41 -0400

    Update Block.java

--- Block.java
+++ Block.java
@@ -1,10 +1,20 @@
 import java.util.ArrayList;
 
-public class Block implements Serializable {
+public class Block {
     int index;
     long timestamp;
     byte[] previousHash=new byte[32];
     ArrayList<Transaction> transactions=new ArrayList<Transaction>();
     int nonce;
-    byte[] thisblockHash=new byte[32];
+    byte[] thisblockHash;
+    public byte[] hashBlock() {
+    }
+    public boolean checkHashZeros(byte[] hash, int numZeros) {
+        for(int i=0; i<numZeros; i++) {
+            if((hash[i]>>(i-1)) & 1)==1) {
+                return false
+            }
+        }
+        return true;
+    }
 }
