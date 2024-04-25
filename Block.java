d83f195655c0d57c769c250345a04c1b47df7ad7
Icy26 <114495528+Icy262@users.noreply.github.com>
2024-04-25 10:50:40 -0400

    Update Block.java

--- Block.java
+++ Block.java
@@ -5,7 +5,7 @@ public class Block {
     long timestamp;
     byte[] previousHash=new byte[32];
     ArrayList<Transaction> transactions=new ArrayList<Transaction>();
-    int nonce;
+    long nonce;
     byte[] thisblockHash;
     public byte[] hashBlock() {
     }
