3de07cdb6943f1c65162345d497e8d142c1672af
Icy26 <114495528+Icy262@users.noreply.github.com>
2024-04-25 09:57:19 -0400

    Update Block.java

--- Block.java
+++ Block.java
@@ -1,6 +1,10 @@
 import java.util.ArrayList;
 
-public class Block {
+public class Block implements Serializable {
+    int index;
+    long timestamp;
     byte[] previousHash=new byte[32];
     ArrayList<Transaction> transactions=new ArrayList<Transaction>();
+    int nonce;
+    byte[] thisblockHash=new byte[32];
 }
