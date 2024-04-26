ad3221cfa3f98fb442c7d17f129566ecf3d3ad32
Icy26 <114495528+Icy262@users.noreply.github.com>
2024-04-26 10:17:21 -0400

    Update Block.java
    Added functionality to convert to byte array

--- Block.java
+++ Block.java
@@ -8,10 +8,25 @@ public class Block {
     long timestamp;
     byte[] previousHash=new byte[32];
     ArrayList<Transaction> transactions=new ArrayList<Transaction>();
+    long nonce;
     byte[] thisblockHash;
-    transient HashBlock hashBlock=new HashBlock(index, timestamp, previousHash, transactions);
+    public byte[] blockAsByteArray() {
+        ByteArrayOutputStream blockAsByteArray=new ByteArrayOutputStream();
+        blockAsByteArray.write(index);
+        blockAsByteArray.write(timestamp);
+        blockAsByteArray.write(previousHash);
+        ObjectOutputStream objectToByte=new ObjectOutputStream(blockAsByteArray); 
+        try {
+            for(int i=0; i<transactions.size(); i++) {
+                objectToByte.writeObject(transactions.get(i));
+            }
+            objectToByte.flush();
+            blockAsByteArray.write();
+        } catch (Exception e) {
+        }
+        blockAsByteArray.write(nonce);
+    }
     public byte[] hashBlock() {
-        
     }
     public boolean checkHashZeros(byte[] hash, int numZeros) {
         for(int i=0; i<numZeros; i++) {
@@ -20,5 +35,11 @@ public class Block {
             }
         }
         return true;
+    }
+    public Block(int index, long timestamp, byte[] previousHash, ArrayList<Transaction> transactions) {
+        this.index=index;
+        this.timestamp=timestamp;
+        this.previousHash=previousHash;
+        this.transactions=transactions;
     }
 }
