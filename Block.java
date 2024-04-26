72e705091ca878ba75714e2a5170d1fa9625b95f
Icy262 <114495528+Icy262@users.noreply.github.com>
2024-04-25 20:40:41 -0400

    changed stuff

--- Block.java
+++ Block.java
@@ -1,18 +1,22 @@
 import java.util.ArrayList;
+import java.security.MessageDigest;
+import java.io.ObjectOutputStream;
+import java.io.ByteArrayOutputStream;
 
 public class Block {
     int index;
     long timestamp;
     byte[] previousHash=new byte[32];
     ArrayList<Transaction> transactions=new ArrayList<Transaction>();
-    long nonce;
     byte[] thisblockHash;
+    transient HashBlock hashBlock=new HashBlock(index, timestamp, previousHash, transactions);
     public byte[] hashBlock() {
+        
     }
     public boolean checkHashZeros(byte[] hash, int numZeros) {
         for(int i=0; i<numZeros; i++) {
-            if((hash[i]>>(i-1)) & 1)==1) {
-                return false
+            if(((hash[i]>>(i-1)) & 1)==1) {
+                return false;
             }
         }
         return true;
