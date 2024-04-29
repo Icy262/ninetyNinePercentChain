826f5c6c1aec2ac5504f3c224ced73698319ed70
Icy26 <114495528+Icy262@users.noreply.github.com>
2024-04-29 10:17:02 -0400

    Blocks will no longer be written to the main directory
    Blocks will now be written to a Blockchain directory instead of the main one with all the code (I'm stupid).

--- BlockFile.java
+++ BlockFile.java
@@ -5,7 +5,7 @@ import java.io.ObjectOutputStream;
 
 class BlockFile {
     public static Block readBlock(int blockNum) {
-        FileInputStream blockFile=new FileInputStream("./"+String.valueOf(blockNum)+".ser");
+        FileInputStream blockFile=new FileInputStream("./blockchain/"+String.valueOf(blockNum)+".ser");
         ObjectInputStream blockReader=new FileInputStream(blockFile);
         Block block=(Block) blockReader.readObject();
         blockReader.close();
@@ -13,7 +13,7 @@ class BlockFile {
         return block;
     }
     public static void writeBlock(Block toWrite) {
-        FileOutputStream blockFile=new FileOutputStream("./"+String.valueOf(toWrite.index)+".ser");
+        FileOutputStream blockFile=new FileOutputStream("./blockchain/"+String.valueOf(toWrite.index)+".ser");
         ObjectOutputStream blockWriter=new ObjectOutputStream(blockFile);
         blockWriter.writeObject(toWrite);
         blockWriter.close();
