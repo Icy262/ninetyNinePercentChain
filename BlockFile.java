8141d7efbf0e7bbd73767ebc0663ac6ba9f6ecc3
Icy26 <114495528+Icy262@users.noreply.github.com>
2024-04-29 10:13:54 -0400

    Allows for reading and writing block to file
    Should work; code hasn't been run yet.

---
+++ BlockFile.java
@@ -0,0 +1,22 @@
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+
+class BlockFile {
+    public static Block readBlock(int blockNum) {
+        FileInputStream blockFile=new FileInputStream("./"+String.valueOf(blockNum)+".ser");
+        ObjectInputStream blockReader=new FileInputStream(blockFile);
+        Block block=(Block) blockReader.readObject();
+        blockReader.close();
+        blockFile.close();
+        return block;
+    }
+    public static void writeBlock(Block toWrite) {
+        FileOutputStream blockFile=new FileOutputStream("./"+String.valueOf(toWrite.index)+".ser");
+        ObjectOutputStream blockWriter=new ObjectOutputStream(blockFile);
+        blockWriter.writeObject(toWrite);
+        blockWriter.close();
+        blockFile.close();
+    }
+}
