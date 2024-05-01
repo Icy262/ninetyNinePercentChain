83416ea25f0dd974aa04cb111117fbd61a5f906f
Icy26 <114495528+Icy262@users.noreply.github.com>
2024-05-01 10:37:40 -0400

    Added DNS registration capability

---
+++ RegisterDNS.java
@@ -0,0 +1,19 @@
+import java.net.ServerSocket;
+import java.lang.Thread;
+import java.io.ObjectOutputStream;
+
+class RegisterDNS extends Thread {
+	public void run() {
+		int port=9937;
+		String DNSIP="ninetyNinePercent.mrman314.tech";
+		try {
+			Socket socket=new Socket(ip, port);
+			ObjectOutputStream write=new ObjectOutputStream(socket.getOutputStream());
+			write.writeObject(socket.getInetAddress().toString());
+			test.close();
+			socket.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
