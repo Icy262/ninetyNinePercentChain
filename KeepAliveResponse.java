30c2559045bb56a199b571de9cb16f857e290307
Icy26 <114495528+Icy262@users.noreply.github.com>
2024-05-01 11:11:43 -0400

    Simplified KeepAliveResponse
    Continuously accepts new responses and then closes them. This   will satisfy KeepAlive, which only checks for a connection.

--- KeepAliveResponse.java
+++ KeepAliveResponse.java
@@ -1,12 +1,13 @@
 import java.net.ServerSocket;
 import java.lang.Thread;
 
-class KeepAliveResponseManager extends Thread {
+class KeepAliveResponse extends Thread {
 	int port=9937;
 	public void run() {
 		ServerSocket serverSocket=new ServerSocket(port);
 		while(true) {
-			new KeepAliveResponseSender(serverSocket.accept()).start();
+			serverSocket.accept();
+			serverSocket.close();
 		}
 	}
 }
