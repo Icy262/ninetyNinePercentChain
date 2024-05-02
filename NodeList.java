9bc7606290e30f4050d75771815c503380f5d938
Icy26 <114495528+Icy262@users.noreply.github.com>
2024-05-02 11:09:16 -0400

    Made NodeList static

--- NodeList.java
+++ NodeList.java
@@ -1,12 +1,9 @@
 class NodeList {
-	private String[] ip;
-	public void setIP(int i, String value) {
+	private static String[] ip;
+	public static void setIP(int i, String value) {
 		ip[i]=value;
 	}
-	public String getIP(int i) {
+	public static String getIP(int i) {
 		return ip[i];
-	}
-	public NodeList(String[] ip) {
-		this.ip=ip;
 	}
 }
