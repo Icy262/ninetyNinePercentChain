d1b373dde5862bbbdb3300a328a545525bc282ff
Icy26 <114495528+Icy262@users.noreply.github.com>
2024-05-02 11:10:25 -0400

    Made QueryDNS actually change the nodelist

--- QueryDNS.java
+++ QueryDNS.java
@@ -6,16 +6,14 @@ class QueryDNS extends Thread {
 	public void run() {
 		int port=9937;
 		String DNSIP="ninetyNinePercent.mrman314.tech";
-		String[] ip=new String[10];
 		try {
 			Socket socket=new Socket(DNSIP, port);
 			ObjectInputStream read=new ObjectInputStream(socket.getInputStream());
 			for(int i=0; i<10; i++) {
-				ip[i]=(String) read.readObject();
+				NodeList.setIP(i, (String) read.readObject());
 			}
 			read.close();
 			socket.close();
-			//ADD ip TO LIST
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
