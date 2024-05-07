2ff573163057bd038e80dbcab76228928b3d6e4a
Icy26 <114495528+Icy262@users.noreply.github.com>
2024-05-07 09:36:24 -0400

    Added long timestamp into constructor

--- Transaction.java
+++ Transaction.java
@@ -5,8 +5,9 @@ public class Transaction implements Serializable {
 	TransactionOut[] TOUT;
 	long timestamp=System.currentTimeMillis();
 
-	public Transaction(TransactionIn[] TIN, TransactionOut[] TOUT) {
+	public Transaction(TransactionIn[] TIN, TransactionOut[] TOUT, long timestamp) {
 		this.TIN=TIN;
 		this.TOUT=TOUT;
+		this.timestamp=timestamp;
 	}
 }
