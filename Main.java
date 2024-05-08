d1c388f61d10d542f1ff2f6e39937910f0f1a436
Icy262 <114495528+Icy262@users.noreply.github.com>
2024-05-08 08:52:00 -0400

    Main now creates threads for background network operations

--- Main.java
+++ Main.java
@@ -1,4 +1,8 @@
 public class Main {
 	public static void main(String[] args) {
+		new RegisterDNS().start();
+		new QueryDNS().start();
+		new KeepAliveResponse().start();
+		new KeepAliveManager().start();
 	}
 }
