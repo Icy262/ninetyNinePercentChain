47edfc515bc30dee5aceb9f39172b30372d146a0
Icy262 <114495528+Icy262@users.noreply.github.com>
2024-05-08 08:55:30 -0400

    Reformatted QueryDNS

--- QueryDNS.java
+++ QueryDNS.java
@@ -6,16 +6,19 @@ class QueryDNS extends Thread {
 	public void run() {
 		int port=9940;
 		String DNSIP="ninetyNinePercent.mrman314.tech";
-		try {
-			Socket socket=new Socket(DNSIP, port);
-			ObjectInputStream read=new ObjectInputStream(socket.getInputStream());
-			for(int i=0; i<10; i++) {
-				NodeIP.addIP((String) read.readObject());
+		while(true) {
+			try {
+				Socket socket=new Socket(DNSIP, port);
+				ObjectInputStream read=new ObjectInputStream(socket.getInputStream());
+				for(int i=0; i<10; i++) {
+					NodeIP.addIP((String) read.readObject());
+				}
+				read.close();
+				socket.close();
+				Thread.sleep(600000);
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
-			read.close();
-			socket.close();
-		} catch (Exception e) {
-			e.printStackTrace();
 		}
 	}
 }
