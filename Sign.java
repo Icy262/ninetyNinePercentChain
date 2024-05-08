582766b2eff505a79aa3715e55a3ea32c41cffed
Icy262 <114495528+Icy262@users.noreply.github.com>
2024-05-08 09:35:55 -0400

    Sign.java now actually signs and returns a byte array

--- Sign.java
+++ Sign.java
@@ -1,21 +1,15 @@
 import java.security.Signature;
-import java.security.KeyPair;
-import java.security.SecureRandom;
-import java.security.KeyPairGenerator;
 
 public class Sign {
-	public static byte[] privateKeySign(byte[] toSign) {
-		byte[] digitalSignature=null;
+	public static byte[] privateKeySign(byte[] toSign, String keyName) {
 		try {
 			Signature signature = Signature.getInstance("SHA256WithDSA");
-			SecureRandom secureRandom = new SecureRandom();
-			KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("DSA");
-			KeyPair keyPair = keyPairGenerator.generateKeyPair();
-			signature.initSign(keyPair.getPrivate(), secureRandom);
+			signature.initSign(KeyPairManager.readKey(keyName).getPrivate());
 			signature.update(toSign);
-			digitalSignature = signature.sign();
+			return signature.sign();
 		} catch(Exception e) {
+			System.out.println(e);
+			return null;
 		}
-		return digitalSignature;
 	}
 }
