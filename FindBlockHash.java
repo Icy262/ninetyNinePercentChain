99a8451d4f3596f18d25cf69dd48c4e139d5263e
Icy262 <114495528+Icy262@users.noreply.github.com>
2024-05-09 13:06:18 -0400

    FindBlockHash now works with updated block

--- FindBlockHash.java
+++ FindBlockHash.java
@@ -2,12 +2,13 @@ import java.nio.ByteBuffer;
 
 public class FindBlockHash extends Thread {
 	public Block run(Block blockToHash, int numZeros) {
-		byte[] blockAsByteArray=blockToHash.blockAsByteArray();
-		ByteBuffer nonce=ByteBuffer.wrap(blockAsByteArray, 12, 8);
+		blockToHash.genMerkleRoot();
+		byte[] headerAsByteArray=blockToHash.headerAsByteArray();
+		ByteBuffer nonce=ByteBuffer.wrap(headerAsByteArray, 12, 8);
 		nonce.asLongBuffer();
 		do {
 			nonce.putLong(12, nonce.getLong(12)+1);
-		} while(!Block.checkHashZeros(SHA256Hash.hash(blockAsByteArray), numZeros));
+		} while(!Block.checkHashZeros(SHA256Hash.hash(headerAsByteArray), numZeros));
 		blockToHash.nonce=nonce.getLong(12);
 		return blockToHash;
 	}
