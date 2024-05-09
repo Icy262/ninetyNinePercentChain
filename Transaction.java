8fb49d47a3bf914453482764cde1f8499a608c2f
Icy262 <114495528+Icy262@users.noreply.github.com>
2024-05-09 13:06:59 -0400

    Transaction header can now be converted to a byte array and hashed.

--- Transaction.java
+++ Transaction.java
@@ -1,9 +1,15 @@
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
 
 public class Transaction implements Serializable, MerkleTreeable {
-	TransactionIn[] TIN;
-	TransactionOut[] TOUT;
-	long timestamp=System.currentTimeMillis();
+	TransactionIn[] TIN; //Not in header
+	TransactionOut[] TOUT; //Not in header
+	byte[] merkleRoot; //Header
+	long timestamp=System.currentTimeMillis(); //Header
+	byte[] signature; //Header
 
 	public Transaction(TransactionIn[] TIN, TransactionOut[] TOUT, long timestamp) {
 		this.TIN=TIN;
@@ -11,10 +17,25 @@ public class Transaction implements Serializable, MerkleTreeable {
 		this.timestamp=timestamp;
 	}
 	public byte[] hash() {
-		return SHA256Hash.hash(blockAsByteArray());
+		return SHA256Hash.hash(headerAsByteArray());
 	}
-	public byte[] blockAsByteArray() {
-		//IMPLEMENT PROPERLY LATER
-		return new byte[32];
+	public byte[] headerAsByteArray() {
+		try {
+			ByteArrayOutputStream headerAsByteArray=new ByteArrayOutputStream();
+			DataOutputStream longWriter=new DataOutputStream(headerAsByteArray);
+			longWriter.writeLong(timestamp);
+			longWriter.flush();
+			headerAsByteArray.write(merkleRoot);
+			return headerAsByteArray.toByteArray();
+		} catch(Exception e) {
+			System.out.println(e);
+			return null;
+		}
+	}
+	public void signTransaction(String keyName) {
+		signature=Sign.privateKeySign(headerAsByteArray(), keyName);
+	}
+	public void genMerkleRoot() {
+		merkleRoot=ByteArray.merge(new MerkleTree<TransactionIn>(new ArrayList<TransactionIn>(Arrays.asList(TIN))).genTree(), new MerkleTree<TransactionOut>(new ArrayList<TransactionOut>(Arrays.asList(TOUT))).genTree());
 	}
 }
