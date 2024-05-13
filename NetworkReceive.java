0bc27801379fd2bee111f9cffec5eaa6b651e897
Icy262 <114495528+Icy262@users.noreply.github.com>
2024-05-13 09:02:45 -0400

    Added support for sending and receiving objects to other nodes

---
+++ NetworkReceive.java
@@ -0,0 +1,18 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.ObjectInputStream;
+
+public class NetworkReceive extends Thread {
+	Socket endpoint;
+	public void run() {
+		try {
+			ObjectInputStream endpointInputStream=new ObjectInputStream(endpoint.getInputStream());
+			endpointInputStream.readObject();
+		} catch(Exception e) {
+			System.out.println(e);
+		}
+	}
+	public NetworkReceive(Socket endpoint) {
+		this.endpoint=endpoint;
+	}
+}
