25ce2fcd701c54138dd1c590433a0f447193ba45
Icy262 <114495528+Icy262@users.noreply.github.com>
2024-05-13 09:04:23 -0400

    Now hashable and merkletreeable

--- TransactionOut.java
+++ TransactionOut.java
@@ -1,6 +1,6 @@
 public class TransactionOut implements MerkleTreeable {
 	byte[] nextTransactionPublicKey;
 	public byte[] hash() {
-		return new byte[32];
+		return SHA256Hash.hash(nextTransactionPublicKey);
 	}
 }
