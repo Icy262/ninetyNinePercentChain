c93726ea4fd41a7dc98748ba79e76a0379fa005c
Icy262 <114495528+Icy262@users.noreply.github.com>
2024-05-13 09:04:09 -0400

    Now hashable and merkletreeable

--- TransactionIn.java
+++ TransactionIn.java
@@ -1,8 +1,17 @@
+import java.io.ByteArrayOutputStream;
+
 public class TransactionIn implements MerkleTreeable {
 	int previousOutBlock; //Block number of previous transaction
 	int previousOutTransaction; //Transaction number of previous transaction
 	int previousOutOutputNumber; //The output number of the previous trasaction
 	public byte[] hash() {
-		return new byte[32];
+		return SHA256Hash.hash(toByteArray());
+	}
+	public byte[] toByteArray() {
+		ByteArrayOutputStream headerAsByteArray=new ByteArrayOutputStream();
+		headerAsByteArray.write(previousOutBlock);
+		headerAsByteArray.write(previousOutTransaction);
+		headerAsByteArray.write(previousOutOutputNumber);
+		return headerAsByteArray.toByteArray();
 	}
 }
