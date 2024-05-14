361df1550435b6c8f9d3fdcfe926feab074aa7ea
Icy262 <114495528+Icy262@users.noreply.github.com>
2024-05-14 08:05:11 -0400

    Added multithreading

--- FindBlockHash.java
+++ FindBlockHash.java
@@ -1,7 +1,9 @@
 import java.nio.ByteBuffer;
 
 public class FindBlockHash extends Thread {
-	public Block run(Block blockToHash, int numZeros) {
+	Block blockToHash;
+	int numZeros;
+	public void run() {
 		blockToHash.genMerkleRoot();
 		byte[] headerAsByteArray=blockToHash.headerAsByteArray();
 		ByteBuffer nonce=ByteBuffer.wrap(headerAsByteArray, 12, 8);
@@ -10,6 +12,9 @@ public class FindBlockHash extends Thread {
 			nonce.putLong(12, nonce.getLong(12)+1);
 		} while(!Block.checkHashZeros(SHA256Hash.hash(headerAsByteArray), numZeros));
 		blockToHash.nonce=nonce.getLong(12);
-		return blockToHash;
+	}
+	public FindBlockHash(Block blockToHash, int numZeros) {
+		this.blockToHash=blockToHash;
+		this.numZeros=numZeros;
 	}
 }
