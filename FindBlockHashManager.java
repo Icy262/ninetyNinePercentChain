fc445b15865f42f5cbddddb06a251437f47b0740
Icy262 <114495528+Icy262@users.noreply.github.com>
2024-05-14 09:51:44 -0400

    Handles the FindBlockHash task
    When valid blocks are found, they are written to disk and propagated to the network. When new transactions are added, the thread is stopped, if running, block is updated, and the thread is restarted.

---
+++ FindBlockHashManager.java
@@ -0,0 +1,17 @@
+public class FindBlockHashManager {
+	private static FindBlockHash search;
+	private static Block block;
+	public static void validBlockFound(Block validBlock) {
+		BlockFile.writeBlock(validBlock);
+		NetworkSendManager.addToQueue(validBlock);
+		block=null;
+		search=null;
+	}
+	public static void addTransaction(Transaction newTransaction) {
+		if(search!=null) {
+			search.interrupt();
+		}
+		block.addTransaction(newTransaction);
+		search=new FindBlockHash(block, 16);
+	}
+}
