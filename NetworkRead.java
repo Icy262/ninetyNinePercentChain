853fbf5fb319a7c15a81f7a53ca3d73a62145ed2
Icy262 <114495528+Icy262@users.noreply.github.com>
2024-05-15 20:46:52 -0400

    Added validity checking

--- NetworkRead.java
+++ NetworkRead.java
@@ -1,11 +1,13 @@
 public class NetworkRead {
 	public static void add(Object toAdd) {
 		if(toAdd.getClass()==Transaction.class) {
-			//CHECK VALIDITY
-			FindBlockHashManager.addTransaction((Transaction) toAdd);
+			if(CheckValidity.checkTransaction((Transaction) toAdd)) {
+				FindBlockHashManager.addTransaction((Transaction) toAdd);
+			}
 		} else if(toAdd.getClass()==Block.class) {
-			//CHECK VALIDITY
-			BlockFile.writeBlock((Block) toAdd);
+			if(CheckValidity.checkBlock((Block) toAdd)) {
+				BlockFile.writeBlock((Block) toAdd);
+			}
 		}
 	}
 }
