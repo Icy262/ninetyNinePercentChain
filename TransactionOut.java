f2c96b7287945707d4c5032f9fc6939d60308b28
Icy262 <114495528+Icy262@users.noreply.github.com>
2024-05-15 20:44:48 -0400

    Added value field

--- TransactionOut.java
+++ TransactionOut.java
@@ -1,5 +1,6 @@
 public class TransactionOut implements MerkleTreeable {
-	byte[] nextTransactionPublicKey;
+	byte[] nextTransactionPublicKey; //Public key that corresponds to the private key that the next transaction must be signed with
+	int value; //Value of transaction
 	public byte[] hash() {
 		return SHA256Hash.hash(nextTransactionPublicKey);
 	}
