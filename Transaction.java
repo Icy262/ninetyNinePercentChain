da438089672b980c7e9295453e75c5812fd43422
Icy262 <114495528+Icy262@users.noreply.github.com>
2024-05-15 20:46:02 -0400

    Added array of signatures for each TxIN and method to generate signatures

--- Transaction.java
+++ Transaction.java
@@ -9,7 +9,7 @@ public class Transaction implements Serializable, MerkleTreeable {
 	TransactionOut[] TOUT; //Not in header
 	byte[] merkleRoot; //Header
 	long timestamp=System.currentTimeMillis(); //Header
-	byte[] signature; //Header
+	byte[][] signature; //Not in header. Contains the merkle root signed by each of the TIN private keys.
 
 	public Transaction(TransactionIn[] TIN, TransactionOut[] TOUT, long timestamp) {
 		this.TIN=TIN;
@@ -25,6 +25,7 @@ public class Transaction implements Serializable, MerkleTreeable {
 			DataOutputStream longWriter=new DataOutputStream(headerAsByteArray);
 			longWriter.writeLong(timestamp);
 			longWriter.flush();
+			genMerkleRoot();
 			headerAsByteArray.write(merkleRoot);
 			return headerAsByteArray.toByteArray();
 		} catch(Exception e) {
@@ -32,8 +33,14 @@ public class Transaction implements Serializable, MerkleTreeable {
 			return null;
 		}
 	}
-	public void signTransaction(String keyName) {
-		signature=Sign.privateKeySign(headerAsByteArray(), keyName);
+	public void signTransaction(String[] keyNames) {
+		try {
+			for(int i=0; i<TIN.length; i++) {
+				signature[i]=Sign.privateKeySign(headerAsByteArray(), KeyPairManager.readKey(keyNames[i]).getPrivate());
+			}
+		} catch(Exception e) {
+			System.out.println(e);
+		}
 	}
 	public void genMerkleRoot() {
 		merkleRoot=ByteArray.merge(new MerkleTree<TransactionIn>(new ArrayList<TransactionIn>(Arrays.asList(TIN))).genTree(), new MerkleTree<TransactionOut>(new ArrayList<TransactionOut>(Arrays.asList(TOUT))).genTree());
