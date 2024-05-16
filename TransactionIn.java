a62dc8b5d2b903a8c1433b9dc0bd08d89ccc97d9
Icy262 <114495528+Icy262@users.noreply.github.com>
2024-05-15 20:45:12 -0400

    Added privateKeySignature and signature method

--- TransactionIn.java
+++ TransactionIn.java
@@ -4,6 +4,7 @@ public class TransactionIn implements MerkleTreeable {
 	int previousOutBlock; //Block number of previous transaction
 	int previousOutTransaction; //Transaction number of previous transaction
 	int previousOutOutputNumber; //The output number of the previous trasaction
+	byte[] privateKeySignature; //Signature with coresponding private key of transaction output
 	public byte[] hash() {
 		return SHA256Hash.hash(toByteArray());
 	}
@@ -13,5 +14,8 @@ public class TransactionIn implements MerkleTreeable {
 		headerAsByteArray.write(previousOutTransaction);
 		headerAsByteArray.write(previousOutOutputNumber);
 		return headerAsByteArray.toByteArray();
+	}
+	public void sign(String keyName) {
+		privateKeySignature=Sign.privateKeySign(toByteArray(), keyName);
 	}
 }
