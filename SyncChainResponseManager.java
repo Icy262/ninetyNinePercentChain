e3588c5736341ffa99a9b58c093d95d50aec3832
Icy262 <114495528+Icy262@users.noreply.github.com>
2024-05-21 10:14:37 -0400

    Listens for incoming connections on port 9942. Accepts the connection and creates a new SyncChainResponder thread to handle the connection.

---
+++ SyncChainResponseManager.java
@@ -0,0 +1,13 @@
+import java.net.ServerSocket;
+
+public class SyncChainResponseManager extends Thread {
+	public void run() {
+		try(ServerSocket endpoint=new ServerSocket(9942)) {
+			while(true) {
+				new SyncChainResponder(endpoint.accept()).start();
+			}
+		} catch(Exception e) {
+			System.out.println(e);
+		}
+	}
+}
