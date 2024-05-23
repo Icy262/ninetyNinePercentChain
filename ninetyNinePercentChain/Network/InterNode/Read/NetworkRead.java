d1254a720d376563f39180ad6e99db94c8d32d34
Icy262 <114495528+Icy262@users.noreply.github.com>
2024-05-23 13:09:05 -0400

    Network read now has a hashing flag. If true, behaviour is unchanged. If false, instead of passing transactions to the hashing manager, we just add it to a queue

--- ninetyNinePercentChain/Network/InterNode/Read/NetworkRead.java
+++ ninetyNinePercentChain/Network/InterNode/Read/NetworkRead.java
@@ -1,5 +1,7 @@
 package ninetyNinePercentChain.Network.InterNode.Read;
 
+import java.util.ArrayList;
+
 import ninetyNinePercentChain.Block.Block;
 import ninetyNinePercentChain.Block.Transaction;
 import ninetyNinePercentChain.Block.Hashing.CheckValidity;
@@ -7,15 +9,25 @@ import ninetyNinePercentChain.Block.Hashing.FindBlockHashManager;
 import ninetyNinePercentChain.Utils.BlockFile;
 
 public class NetworkRead {
+	private static boolean hashing;
+	private static ArrayList<Transaction> queue;
 	public static void add(Object toAdd) {
 		if(toAdd.getClass()==Transaction.class) {
-			if(CheckValidity.checkTransaction((Transaction) toAdd)) {
+			if(CheckValidity.checkTransaction((Transaction) toAdd)&&hashing==true) {
 				FindBlockHashManager.addTransaction((Transaction) toAdd);
+			} else {
+				queue.add((Transaction)toAdd);
 			}
 		} else if(toAdd.getClass()==Block.class) {
 			if(CheckValidity.checkBlock((Block) toAdd)) {
 				BlockFile.writeBlock((Block) toAdd);
 			}
 		}
+	}
+	public static Transaction getQueue() {
+		return queue.remove(0);
+	}
+	public static void setHashing(boolean hashing) {
+		NetworkRead.hashing=hashing;
 	}
 }
