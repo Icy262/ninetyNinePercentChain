cb23fbcc07e0e98134981a6ffbe122495b312b75
Icy262 <114495528+Icy262@users.noreply.github.com>
2024-05-31 10:50:42 -0400

    Added more comments

--- ninetyNinePercentChain/Network/NodeIP.java
+++ ninetyNinePercentChain/Network/NodeIP.java
@@ -10,8 +10,8 @@ public class NodeIP {
 	/*
 	Name: addIP
 	Description: Checks if the list of IP addresses contains the ip. If it doesn't, we add the ip to the list, and update the NetworkSendManager and sync our version of the blockchain with them.
-	Precondition:
-	Postcondition:
+	Precondition: None
+	Postcondition: IP added to list if not already there and new SyncChain thread started
 	*/
 	public static void addIP(String ip) {
 		if(!nodeIPs.contains(ip)) {
@@ -23,36 +23,36 @@ public class NodeIP {
 	/*
 	Name: removeIP
 	Description: Removes the IP from our list and updates the NetworkSendManager
-	Precondition:
-	Postcondition:
+	Precondition: None
+	Postcondition: IP removed, if present
 	*/
 	public static void removeIP(String ip) {
 		nodeIPs.remove(ip);
 		NetworkSendManager.update();
 	}
 	/*
-	Name:
-	Description:
-	Precondition:
-	Postcondition:
+	Name: getIP
+	Description: Gets the ip at index index
+	Precondition: There is an index index
+	Postcondition: The IP at index index is returned
 	*/
 	public static String getIP(int index) {
 		return nodeIPs.get(index);
 	}
 	/*
-	Name:
-	Description:
-	Precondition:
-	Postcondition:
+	Name: getSize
+	Description: Gets the size of the nodeIPs array
+	Precondition: None
+	Postcondition: nodeIPs.size returned
 	*/
 	public static int getSize() {
 		return nodeIPs.size();
 	}
 	/*
-	Name:
-	Description:
-	Precondition:
-	Postcondition:
+	Name: getNextIP
+	Description: Keeps a rolling index. This lets us loop through the ip addresses. For the DNS server, this allows us to make sure that we are distributing all our ips equally. If not, we might have 100 ips, but we only ever share the first 10.
+	Precondition: None
+	Postcondition: Returns the next ip in the list
 	*/
 	public static String getNextIP() {
 		currentIndex=currentIndex++%nodeIPs.size();
--- ninetyNinePercentChain/NetworkTransaction/InsufficientFundsException.java
+++ ninetyNinePercentChain/NetworkTransaction/InsufficientFundsException.java
@@ -1,6 +1,12 @@
 package ninetyNinePercentChain.NetworkTransaction;
 
 class InsuffientFundsException extends Exception {
+	/*
+	Name: InsufficientFunsException
+	Description: Thrown when there are not enough funds in an address to make a transaction
+	Precondition: None
+	Postcondition: None
+	*/
 	public InsuffientFundsException() {
 		super();
 	}
