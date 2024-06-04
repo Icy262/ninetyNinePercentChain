bba18f64e0dac190fb3ebf68d37716d854a89550
Icy262 <114495528+Icy262@users.noreply.github.com>
2024-06-04 13:44:33 -0400

    Now checks the sender, receiver, and amount are correct

--- ninetyNinePercentChain/NetworkTransaction/WaitForTransactionManager.java
+++ ninetyNinePercentChain/NetworkTransaction/WaitForTransactionManager.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 import ninetyNinePercentChain.Block.Block;
+import ninetyNinePercentChain.Block.TransactionIn;
+import ninetyNinePercentChain.Block.TransactionOut;
+import ninetyNinePercentChain.Utils.BlockFile;
 
 public class WaitForTransactionManager {
 	private static ArrayList<WaitForTransaction> waiting=new ArrayList<WaitForTransaction>();
@@ -26,10 +29,30 @@ public class WaitForTransactionManager {
 		if(waiting.size()==0) {
 			return;
 		}
-		for(int i=0; i<waiting.size(); i++) {
-			for(int ii=0; ii<block.getTOUTLength(); ii++) {
-				if(Arrays.equals(waiting.get(i).getAddress(), block.getTOUT(ii).getNextTransactionPublicKey())&&waiting.get(i).getValue()==block.getTOUT(ii).getValue()) {
-					waiting.get(i).notify();
+		for(int i=0; i<waiting.size(); i++) { //For each transaction waiting,
+			for(int ii=0; ii<block.getNumTransactions(); ii++) { //For each transaction in the block,
+				for(int iii=0; i<block.getTransaction(ii).getTOUTLength(); iii++) { //For each TOUT in the transaction,
+					boolean sendAddressMet=false;
+					boolean recieveAddressMet=false;
+					boolean amountMet=false;
+					WaitForTransaction currentWaiting=waiting.get(i);
+					TransactionOut TOUT=block.getTransaction(ii).getTOUT(iii);
+					TransactionIn TIN=block.getTransaction(ii).getTIN(iii);
+					for(int iiii=0; iiii<block.getTransaction(ii).getTINLength(); iiii++) {
+						if(Arrays.equals(BlockFile.getTOUT(TIN.getPreviousOutBlock(), TIN.getPreviousOutTransaction(), TIN.getPreviousOutOutputNumber()).getNextTransactionPublicKey(), currentWaiting.getSendAddress())) {
+							sendAddressMet=true;
+							break;
+						}
+					}
+					if(Arrays.equals(TOUT.getNextTransactionPublicKey(), currentWaiting.getRecieveAddress())) {
+						recieveAddressMet=true;
+					}
+					if(TOUT.getValue()==currentWaiting.getValue()) {
+						amountMet=true;
+					}
+					if(sendAddressMet&&recieveAddressMet&&amountMet) {
+						waiting.get(i).notify();
+					}
 				}
 			}
 		}
