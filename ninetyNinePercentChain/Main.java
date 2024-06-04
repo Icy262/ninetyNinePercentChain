d940f7073f31c8ecb7f3fe59584dd9e94b8ae585
Icy262 <114495528+Icy262@users.noreply.github.com>
2024-06-04 09:59:34 -0400

    Typing "STOP" into the console now stops all the threads and quits the program

--- ninetyNinePercentChain/Main.java
+++ ninetyNinePercentChain/Main.java
@@ -1,4 +1,7 @@
 package ninetyNinePercentChain;
+
+import java.util.Scanner;
+
 import ninetyNinePercentChain.Network.DNS.QueryDNS;
 import ninetyNinePercentChain.Network.DNS.RegisterDNS;
 import ninetyNinePercentChain.Network.InterNode.Read.NetworkReceiveHandler;
@@ -8,11 +11,32 @@ import ninetyNinePercentChain.Network.KeepAlive.KeepAliveResponse;
 
 public class Main {
 	public static void main(String[] args) {
-		new RegisterDNS().start(); //Starts RegisterDNS thread. This will tell the DNS server that we are a node and we are online
-		new QueryDNS().start(); //Starts the QueryDNS thread. This will query the DNS server for new IP adresses every 10 minutes or if there was no IPs returned last time, 1 second.
-		new KeepAliveResponse().start(); //Starts the KeepAliveResponse thread. This will respond to any incoming keep alive requests by starting a new KeepAliveResponse thread.
-		new KeepAliveManager().start(); //Starts the KeepAliveManager thread. This will send out keep alive messages to all the IPs in our list every five minutes. If they don't accept the connection within 10 seconds, we delete their ip from our list.
-		new NetworkReceiveHandler(true).start(); //Starts the NetworkRecieveHandler thread with the hashing flag set to true. This will accept any incoming data and direct it to the hashing manager.
-		new SyncChainResponseManager().start(); //Starts the SyncChainReponseManager. This will sync our copy of the blockchain with any new IPs that we add to our list.
+		QueryDNS queryDNS=new QueryDNS(); //Creates a QueryDNS object
+		KeepAliveResponse keepAliveResponse=new KeepAliveResponse(); //Creates a KeepAliveResponse object
+		KeepAliveManager keepAliveManager=new KeepAliveManager(); //Creates a KeepAliveManager object
+		NetworkReceiveHandler networkReceiveHandler=new NetworkReceiveHandler(true); //Creates a NetworkRecieveHandler object with hashing flag set to true
+		SyncChainResponseManager syncChainResponseManager=new SyncChainResponseManager(); //Creates a SyncChainResponseManager object
+		
+		new RegisterDNS().start(); //Starts the RegisterDNS thread. This will tell the DNS server that we are a node and we are online.
+		queryDNS.start(); //Starts the QueryDNS thread. This will query the DNS server for new IP adresses every 10 minutes or if there was no IPs returned last time, 1 second.
+		keepAliveResponse.start(); //Starts the KeepAliveResponse thread. This will respond to any incoming keep alive requests by starting a new KeepAliveResponse thread.
+		keepAliveManager.start(); //Starts the KeepAliveManager thread. This will send out keep alive messages to all the IPs in our list every five minutes. If they don't accept the connection within 10 seconds, we delete their ip from our list.
+		networkReceiveHandler.start(); //Starts the NetworkRecieveHandler thread with the hashing flag set to true. This will accept any incoming data and direct it to the hashing manager.
+		syncChainResponseManager.start(); //Starts the SyncChainReponseManager. This will sync our copy of the blockchain with any new IPs that we add to our list.
+		
+		Scanner userInput=new Scanner(System.in); //Scanner.
+		boolean stopProgram=false; //Flag. If false, program continues running. If true, program stops.
+		while(!stopProgram) { //While stopProgram is false.
+			System.out.println("Type \"STOP\" to stop the program");
+			if(userInput.nextLine().equalsIgnoreCase("STOP")); { //If user enters "STOP",
+				stopProgram=true; //Sets the stopProgram flag to true. This will stop the program
+				userInput.close(); //Closes the Scanner
+				queryDNS.stopThread(); //Stops the queryDNS thread
+				keepAliveResponse.stopThread(); //Stops the keepAliveResponse thread
+				keepAliveManager.stopThread(); //Stops the keepAliveManager thread
+				networkReceiveHandler.stopThread(); //Stops the networkReceiveHandler thread
+				syncChainResponseManager.stopThread(); //Stops the syncChainRepsponseManager
+			}
+		}
 	}
 }
